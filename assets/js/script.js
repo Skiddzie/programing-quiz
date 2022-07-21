f0bb90e0e6c392f797638f8899f85ac47250f1ca
Skiddzie <aidanthe1@gmail.com>
2022-07-20 22:16:32 -0400

    starting over

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,111 +1,33 @@
-//note to future aidan: this is all from a youtube tutorial and none of it works because the currentQuestions variable has nothing to do with the questions
-//array. I have no fucking clue how he got this shit to work as it's typed out exactly the same way he did it. so just end up changing all of this
-//when you get back to this project
-
-const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorAll('.choice-text'));
-
-let currentQuestions = {};
-let acceptingAnswers = true;
 let score = 0;
-let questionCounter = 0;
-let availableQuestions = [];
 
-let questions = [
-    {
-        question: 'what is the answer',
-        choice1: "1",
-        choice2: "2",
-        choice3: "3",
-        answer: 1,
+const buttonA1 = document.querySelector(".a1");
+const buttonA2 = document.querySelector(".a2");
+const buttonA3 = document.querySelector(".a3");
+const buttonA4 = document.querySelector(".a4");
 
-    },
-    {
-        question: 'how about this one?',
-        choice1: "1",
-        choice2: "2",
-        choice3: "3",
-        answer: 3,
+const buttonB1 = document.querySelector(".b1");
+const buttonB2 = document.querySelector(".b2");
+const buttonB3 = document.querySelector(".b3");
+const buttonB4 = document.querySelector(".b4");
 
-    },
-    {
-        question: 'what about now?',
-        choice1: "1",
-        choice2: "2",
-        choice3: "3",
-        answer: 3,
-    },
-    {
-        question: 'now?',
-        choice1: "1",
-        choice2: "2",
-        choice3: "3",
-        answer: 2,
+const buttonC1 = document.querySelector(".c1");
+const buttonC2 = document.querySelector(".c2");
+const buttonC3 = document.querySelector(".c3");
+const buttonC4 = document.querySelector(".c4");
 
-    },
-    {
-        question: 'still know the answer, huh?',
-        choice1: "1",
-        choice2: "2",
-        choice3: "3",
-
-    },
-]
-
-const SCORE_POINTS = 100;
-const MAX_QUESTIONS = 4;
-
-startGame = () => {
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = [...questions];
-    getNewQuestion();
-}
-
-getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS){
-        localStorage.setItem("mostRecentScore", score);
-        
-        return window.location.assign('/end.html');
-    }
-
-    questionCounter++;
-    proogressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
-
-    const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionsIndex];
-    question.innerText = currentQuestion.question;
-
-    choices.forEach(choice => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
-    })
-
-    availableQuestions.splice(questionsIndex, 1);
-
-    acceptingAnswers = true;
-}
-
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return;
-
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset['number'];
-
-        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' :
-        'incorrect';
-
-        if(classToApply === 'correct') {
-            incrementScore(SCORE_POINTS);
-        }
-
-        selectedChoice.parentElement.classList.add(classToApply);
-
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply);
-            getNewQuestion()
-        }, 1000)
-    })
+buttonA1.addEventListener('click', e => {
+    score +=1;
+    console.log(score);
+})
+buttonA2.addEventListener('click', e => {
+    score -=1;
+    console.log(score);
+})
+buttonA3.addEventListener('click', e => {
+    score -=1;
+    console.log(score);
+})
+buttonA4.addEventListener('click', e => {
+    score -=1;
+    console.log(score);
 })
